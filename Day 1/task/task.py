af70872c2532c054da5cb830976f910e0fc7e553
MaGo1024 <olumideodusote@gmail.com>
2025-12-02 03:06:43 +0000

    day 1 tasks

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,4 @@
-print("My name is" + " " + "Angela" + "!")
-
-# This line of code will take an input using the input function
-print("Hello " + input("What is your name? ") + "!")
+print("Welcome to the Band Name Generator.\n")
+city = input("what's the name of the city you grew up?\n")
+pet = input("what's the name of your pet?\n")
+print("The name of your band is " + city + pet)
