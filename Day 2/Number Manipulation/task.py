f61cbaa05530b94235c8bd2f47ef1458e839d782
MaGo1024 <olumideodusote@gmail.com>
2025-12-03 00:31:12 +0000

    day 2 tasks

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,6 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+print(round(bmi))
+print(round(bmi,2))
+
+print(f"Yor bmi score is {round(bmi)}")
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,6 @@
-len(12345)
+print(type(12345))
+print(type("12345"))
+print(type(123.45))
+print(type(True))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
