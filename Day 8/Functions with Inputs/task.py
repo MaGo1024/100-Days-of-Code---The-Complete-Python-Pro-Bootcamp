d2057834f77a0b47638e1f198e75c23e4b082635
MaGo1024 <olumideodusote@gmail.com>
2025-12-20 02:51:27 +0000

    Day 8 tasks

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,12 @@
+def greet():
+    print("Hello")
+    print("World")
+    print("!")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("olumide")
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,14 @@
 # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
 
+def greet_with(name, location):
+    return f"Hello {name} \nWhat is it like in {location}"
 
-greet_with_name("Jack Bauer")
+greetings = greet_with(location="England", name="Olumide")
+print(greetings)
