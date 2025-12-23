cd652a408090d7a7e463625cd666e86935f2cbd8
MaGo1024 <olumideodusote@gmail.com>
2025-12-23 03:02:21 +0000

    Day 9 tasks and Blind Auction Project

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,36 @@
 # TODO-1: Ask the user for input
+
+from art import logo
+print(logo)
+
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+from art import logo
+print(logo)
+
+bids = {}
+auction = True
+
+while auction:
+    name = input("What is your name? ")
+    bid = int(input("What's your bid? $"))
+    bids[name] = bid
+
+    continue_bidding = input("Is there a new bid? Type 'yes' or 'no': ").lower()
+    print("\n" * 20)
+
+    if continue_bidding == "no":
+        auction = False
 
 
+# Find the highest bidder
+highest_bid = 0
+winner = ""
+
+for bidder, amount in bids.items():
+    if amount > highest_bid:
+        highest_bid = amount
+        winner = bidder
+
+print(f"The highest bidder is {winner} with ${highest_bid}")
---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,38 @@
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+}
+
+# See if you can figure out how to print out "Lille" from the nested List called travel_log.
+print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1])
+
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+print(travel_log["Germany"]["cities_visited"][2])
+
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
+
+
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
+
+final_dictionary = starting_dictionary["c"] = 7
+
+print(final_dictionary)
