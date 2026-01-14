592f9620b06691c7b21e59722f71ed0a1e44cf39
MaGo1024 <olumideodusote@gmail.com>
2026-01-14 03:32:38 +0000

    Higher or Lower Project

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,26 +1,63 @@
-import random
-from random import randint
-from game_data import *
+# Display art.
 from art import *
+from game_data import data
+import random
 
-"""Pick two random artist for the data dictionary and 
-guess who has more followers.
-if you are correct, game continues,the 2nd artist stay
-and you guess followers against another random artist.
-store you score in a counter
-if you are wrong, the game ends. your final score is displayed.
-"""
-def highest_followers(user_1, user_2):
-    if user_1["follower_count"] > user_2["follower_count"]:
-        return user_1
+def format_data(account):
+    """Takes the account data and returns the printable format."""
+    account_name = account['name']
+    account_desc = account['description']
+    account_country = account['country']
+    return f"{account_name}, a  {account_desc}, from {account_country}"
+
+def check_answers(user_guess, a_followers, b_followers):
+    """Takes the user guess and the follower count of account a dnd b and return if user guesses correctly."""
+    if a_followers > b_followers:
+        return user_guess == "a"
     else:
-        return user_2
-#1. Pick two random artist from data
-artist_1 = random.choice(data)
-print(artist_1['name'])
-artist_2 = random.choice(data)
-print(artist_2)
-#2. Guess who has more followers
-#3. if guess is correct, game continues,increase score counter by 1, move artist to var 1 and select another random artist in var 2.
-#4. else display final score and end game.
+        return user_guess == "b"
 
+print(logo)
+# Generate a random account from the game data.
+score = 0
+game_continue = True
+account_b = random.choice(data)
+
+# Make the game repeatable
+while game_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}.")
+
+    # Ask user for a guess.
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    # Clear the screen
+    print("\n" * 20)
+    print(logo)
+
+    # Check if user is correct.
+    ## Get follower count for each account
+    a_follower_count = account_a['follower_count']
+    b_follower_count = account_b['follower_count']
+    ## Use if statement to check if user is correct.
+    is_correct = check_answers(guess, a_follower_count, b_follower_count)
+    # print(is_correct)
+
+    # Give user feedback on their  guess.
+
+    # Score keeping
+
+    if is_correct:
+        score += 1
+        print(f"You're right!, Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_continue = False
+
+    # Making the account at position B become the next account at position A.
